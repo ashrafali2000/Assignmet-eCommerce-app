9c57630d71e8a3d5206696fe800c4667bed8f585
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-21 16:58:27 +0500

    form update

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,22 +1,57 @@
-
-export default function Form() {
+import Image from "next/image"
+import logo from "../../../public/logo.png"
+import Link from "next/link"
+export default function Form({signUp}) {
     return (
       <>
       
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
+            <Image
+            style={{width:190,height:80}}
               className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              src={logo}
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+           { signUp? "Sign Up for new account" :"Sign in to your account"}
             </h2>
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
+          {signUp ? <div>
+            <div>
+                <label htmlFor="fname" className="block text-sm font-medium leading-6 text-gray-900">
+                  First Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="fname"
+                    name="fname"
+                    type="email"
+                    autoComplete="fname"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="lname" className="block text-sm font-medium leading-6 text-gray-900">
+                  Last Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="lname"
+                    name="lname"
+                    type="lname"
+                    autoComplete="lname"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div> 
+              </div> :""}
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                   Email address
@@ -38,11 +73,7 @@ export default function Form() {
                   <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                     Password
                   </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
+                 
                 </div>
                 <div className="mt-2">
                   <input
@@ -55,22 +86,50 @@ export default function Form() {
                   />
                 </div>
               </div>
+
+            {signUp ?  <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="usrImg" className="block text-sm font-medium leading-6 text-gray-900">
+                    Your Image URL
+                  </label>
+                 
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="usrImg"
+                    name="usrImg"
+                    type="url"
+                    autoComplete="usrImg"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div> : ""}
   
               <div>
-                <button
+            {signUp ?  <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Sign Up
                 </button>
+                : <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign In
+                </button>}
               </div>
             </form>
   
             <p className="mt-10 text-center text-sm text-gray-500">
               Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
+              {signUp ? <Link href="/signIn" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+               sign In
+              </Link>
+              :<Link href="/signUp" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+               sign Up
+              </Link>}
             </p>
           </div>
         </div>
