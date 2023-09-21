94eb33eef7b8748f7457b93f19d6a01638665df8
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-21 16:58:08 +0500

    updatre home component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Navbar from '@/components/navBar'
+import {UserContext} from "../context/userContext"
+import { useState } from 'react'
+import AuthHandler from '@/context/authHandler';
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
+  const [userLogged, setUserLogged] = useState(true);
+
   return (
     <div>
+  <UserContext.Provider value ={ {
+      isloggedIn : userLogged
+    }}>
+     <AuthHandler></AuthHandler>
+  </UserContext.Provider>
       <Navbar></Navbar>
       <h1>Home Page</h1>
     </div>
